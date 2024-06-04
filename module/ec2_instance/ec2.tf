e605e52e5223c6b9965760ee867f2bf920bc4da9
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-04 20:12:21 +0530

    Update ec2.tf

--- module/ec2_instance/ec2.tf
+++ module/ec2_instance/ec2.tf
@@ -1,17 +1,18 @@
-resource "aws_instance" "this_aws_instance" {
+resource "aws_instance" "my_aws_instance" {
   ami                    = var.my_aws_instance_ami
   instance_type          = var.my_aws_instance_instance_type
   key_name               = var.my_aws_instance_key_name
-  vpc_security_group_ids =  [aws_security_group.my_sg.id]
-//[var.this_aws_instance_sg_id]
-  availability_zone      = var.my_aws_instance_availability_zone
-  subnet_id = var.my_aws_instance_subnet 
+  vpc_security_group_ids = aws_security_group.my_sg.id
+  //[var.this_aws_instance_sg_id]
+  availability_zone = var.my_aws_instance_availability_zone
+  subnet_id         = "${module.vpc.subnet_id}"
+  
   root_block_device {
     volume_size = var.my_aws_instance_volume_size
   }
   count = var.my_aws_instance_count
   //user_data_base64 = var.this_aws_instance_user_data_base64
-    user_data = <<-EOF
+  user_data = <<-EOF
             #!/bin/bash
     sudo -i
     yum update -y
@@ -23,21 +24,21 @@ resource "aws_instance" "this_aws_instance" {
 
 
   tags = {
-      
-      Name =  var.my_aws_instance_tags
+
+    Name = var.my_aws_instance_tags
   }
 }
 
 resource "aws_security_group" "my_sg" {
   vpc_id = var.my_sg_vpc_id
   dynamic "ingress" {
-    for_each = [ 22, 80, 443 ,3306, 8080 ]
+    for_each = [22, 80, 443, 3306, 8080]
     iterator = port
     content {
       description = "TLC from VPC"
-      from_port = port.value
-      to_port = port.value
-      protocol = "tcp"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
@@ -47,4 +48,5 @@ resource "aws_security_group" "my_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 }
