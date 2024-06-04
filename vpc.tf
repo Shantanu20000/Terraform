c0384e6c097f5965a036b18ee1eb67584aaa6aee
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-04 19:08:23 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
    region = "us-east-1"
-   access_key = "AKIAR5N5RCH7FOABEYXE"
-   secret_key = "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"
+   access_key = ""
+   secret_key = ""
  }
 
 resource "aws_vpc" "main" {
