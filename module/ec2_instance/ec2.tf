959a54095e3b51f1af80a3af884731d18d203122
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-05 17:09:14 +0530

    Update ec2.tf

--- module/ec2_instance/ec2.tf
+++ module/ec2_instance/ec2.tf
@@ -27,24 +27,3 @@ resource "aws_instance" "my_aws_instance" {
   }
 }
 
-resource "aws_security_group" "my_sg" {
-  vpc_id = var.my_sg_vpc_id
-  dynamic "ingress" {
-    for_each = [22, 80, 443, 3306, 8080]
-    iterator = port
-    content {
-      description = "TLC from VPC"
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
