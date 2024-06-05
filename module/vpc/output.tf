84665323fdb1af52baf9d10facdd35d91838181c
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-05 16:59:43 +0530

    Create output.tf

---
+++ module/vpc/output.tf
@@ -0,0 +1,9 @@
+output "subnet_id" {
+  value = aws_subnet.my_subnet_pub.id
+}
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+output "security_group_id" {
+  value = asw_security_group.my_sg.id
+}
