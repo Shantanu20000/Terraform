40b56db8c91fbf4f61a9fb4bf6c4f7d43c1a60f8
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-06 11:04:59 +0530

    Create provisioner.tf

---
+++ provisioner.tf
@@ -0,0 +1,13 @@
+connection {
+  type = "ssh"
+  user = "ec2-user"
+  private_key = file("${path.module}/id.rsa") # id_rsa is present in same dir in which we command "terraform apply" is apply.
+  host = "${self.public_ip}"
+provisioner "file" {
+  source = "readme.md" # terraform machine
+  destination = "/tmp/readme.md" # remote machine
+  }
+provisioner "file" {
+  content = "this is Terraform User" # terraform machine
+  destination = "/tmp/terraform" # remote machine
+  }
