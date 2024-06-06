f97eef3cb45f156ee624c0750cc56454c24c003a
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-06 11:43:09 +0530

    Update provisioner.tf

--- provisioner.tf
+++ provisioner.tf
@@ -3,6 +3,7 @@ connection {
   user = "ec2-user"
   private_key = file("${path.module}/id.rsa") # id_rsa is present in same dir in which we command "terraform apply" is apply.
   host = "${self.public_ip}"
+  }
 provisioner "file" {
   source = "readme.md" # terraform machine
   destination = "/tmp/readme.md" # remote machine
