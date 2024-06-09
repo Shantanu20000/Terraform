1442a1dffdc11fb6494f65e46f95bd291322c053
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-09 14:29:06 +0530

    Update main.tf

--- module/loadbalancer/main.tf
+++ module/loadbalancer/main.tf
@@ -12,8 +12,8 @@ resource "aws_alb" "alb" {
 resource "aws_lb_target_group" "target_group" {
   name        = var.target_group_name
   port        = 80
-  protocol    = var.protocol
-  vpc_id      = var.vpc_id  
+  protocol    = var.my_protocol
+  vpc_id      = var.my_vpc_id  
   target_type = "instance"
     health_check {
     enabled             = true
@@ -31,18 +31,19 @@ resource "aws_lb_target_group" "target_group" {
 resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_alb.alb.arn
   port              = 80
-  protocol          = var.protocol
+  protocol          = var.my_protocol
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target_group.arn
   }
 }
 
-# resource "aws_lb_target_group_attachment" "target_attachment" {
-#   # count = length(module.my_instances1.instance_ids)
-#   target_group_arn = aws_lb_target_group.target_group.arn
-#   target_id        = aws_lb_target_group.target_group.id # Replace with your target instance ID
-# }
+resource "aws_lb_target_group_attachment" "target_attachment" {
+  count            = min(length(var.my_instance_id), 2)
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = var.my_instance_id[count.index]
+}
+
 
 # resource "aws_lb_target_group_attachment" "target_attachment" {
 #   target_group_arn = aws_lb_target_group.target_group.arn
